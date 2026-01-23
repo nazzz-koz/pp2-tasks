e6925d8aaf866400e2f78a7bbc3c25e02d273432
nazzz-koz <a.nazym98@gmail.com>
2026-01-23 12:09:30 +0500

    working

--- practice01/variables.py
+++ practice01/variables.py
@@ -2,8 +2,8 @@
 a = 4  
 a = "Sally" #initial value has changed
 print(type(a)) #shows data type
-x, y, z = "Orange", "Banana", "Cherry" #let assign multiple values at the same time
-x = y = z = "Orange" #assign one value to 3 vars
+x, y, z = "good", "better", "the best" #let assign multiple values at the same time
+x = y = z = "better" #assign one value to 3 vars
 
 x = str(3)    # x will be '3'
 y = int(3)    # y will be 3
@@ -11,11 +11,11 @@ z = float(3)  # z will be 3.0
 
 
 #Variable name rules
-myvar = "John" # var must start with a letter or underscore
-my_var = "John" # var cannot be a part of the Python keyboard
-_my_var = "John" # var can only contain alpha-numeric characters and underscores
-MYVAR = "John" # var names are case-sensitive
-myvar2 = "John" # var name cannot start with a number
+myvar = "D" # var must start with a letter or underscore
+my_var = "D" # var cannot be a part of the Python keyboard
+_my_var = "D" # var can only contain alpha-numeric characters and underscores
+MYVAR = "D" # var names are case-sensitive
+myvar2 = "D" # var name cannot start with a number
 
 
 #Unpack a collection
