620df6278374888155efee980152e4356dfbde65
nazzz-koz <a.nazym98@gmail.com>
2026-01-23 10:41:31 +0500

    Data types

---
+++ practice01/data_types.py
@@ -0,0 +1,17 @@
+#Data types in Python
+
+q = "Hello" #str > str("")
+w = 20 #int > int()
+e = 20.5 #float > float()
+r = 1j #complex > complex()
+t = ["lemon", "tree", "musin"] #list > list(("",""))
+y = ("lemon", "tree", "musin") #tuple > tuple(("", ""))
+u = range(8) #range > range()
+i = {"band" : "Arctic Monkeys", "song" : "Wanna be yours"} #dict > dict(="")
+o = {"lemon", "tree"} #set > set(("", ""))
+p = frozenset({"lemon", "tree"}) #frozenset 
+a = True #bool > bool()
+s = b"Hello" #bytes > bytes()
+d = bytearray(6) #bytearray
+f = memortview(bytes(7)) #memory view
+g = None #NoneType
