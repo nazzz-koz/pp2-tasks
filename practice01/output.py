bf471de14f58994fd1c7bba8e4eeb9799689fc7c
nazzz-koz <a.nazym98@gmail.com>
2026-01-23 10:12:07 +0500

    learning output

---
+++ practice01/output.py
@@ -0,0 +1,14 @@
+"""Rules of outputting text"""
+print("I am learning Python.") #text must be surrounded by quotes either double or single
+
+print("Hello World!", end=" ") #end is used to not separate text with a space
+print("I will print on the same line.")
+
+"""Rules of outputting numbers"""
+print(358) #dont need quotes
+
+print(3 + 3) #can perform operations
+print(2 * 5)
+
+#Combination of both
+print("I am", 35, "years old.") 
